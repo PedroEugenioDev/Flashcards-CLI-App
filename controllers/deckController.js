083a6d2944561dd0fee52a48aaefc4394cef3460
Pedro Machado <pedroeugenio.dev@gmail.com>
2024-04-15 15:40:12 -0300

    Function to display a menu for each deck created

--- controllers/deckController.js
+++ controllers/deckController.js
@@ -32,18 +32,69 @@ async function showDecks() {
       message: "Select a deck:",
       choices: [...listName, "Back"],
     })
-    .then((answer) => {
+    .then(async (answer) => {
       if (answer["deckOption"] === "Back") {
         return;
       }
-      deckMenu(answer["deckOption"]);
+      await deckMenu(answer["deckOption"]);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
-async function deckMenu() {}
+async function deckMenu(deckName) {
+  console.clear();
+  await inquirer
+    .prompt({
+      type: "list",
+      name: "deckMenuOption",
+      message: `Select an option to ${deckName}:`,
+      choices: [
+        { name: "Add a card", value: 1 },
+        { name: "Review cards", value: 2 },
+        { name: "Delete card", value: 3 },
+        { name: "Exit", value: 4 },
+      ],
+    })
+    .then(async (answer) => {
+      console.clear();
+      switch (answer.deckMenuOption) {
+        case 1:
+          console.log("Create Card");
+          //await createCard();
+          deckMenu();
+          break;
+        case 2:
+          console.log("review Cards");
+          //await reviewCards();
+          deckMenu();
+          break;
+        case 3:
+          console.log("Delete Card");
+          //await deleteCards();
+          deckMenu();
+          break;
+        case 4:
+          deckMenu();
+        default:
+          console.clear();
+          console.log("Invalid option... (Press Enter)");
+          process.stdin.on("keypress", (key) => {
+            console.log({ key });
+            if (key == "\r") {
+              console.clear();
+              main();
+            } else {
+              console.log("Press enter key");
+            }
+          });
+      }
+    })
+    .catch(() => {
+      console.log("Error at Deck Menu");
+    });
+}
 
 async function deleteDesk() {
   console.clear();
